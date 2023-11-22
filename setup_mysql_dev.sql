3c430266c301dfd212dca3f63699b186ca3ea4ea
lelenguyasameria <somearia711@gmail.com>
2023-11-22 13:34:55 -0800

    setupenvi

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,40 +1,36 @@
-#!/bin/bash
+-- Creates a MySQL server with:
+--   Database hbnb_dev_db.
+--   User hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grants all privileges for hbnb_dev on hbnb_dev_db.
+--   Grants SELECT privilege for hbnb_dev on performance.
 
-# MySQL server configuration
-MYSQL_ROOT_USER="root"
-MYSQL_ROOT_PASSWORD="your_root_password"
+-- Connect to the MySQL server as root (adjust the credentials if needed)
+db = hbnb_dev.connect(
+    host='localhost',
+    port=3306,
+    user='hbnb_dev',
+    passwd='hbnb_dev_pwd'
+)
 
-# Database and user configuration
-DB_NAME="hbnb_dev_db"
-DB_USER="hbnb_dev"
-DB_PASSWORD="hbnb_dev_pwd"
+-- Create a cursor object to execute queries
+cursor = db.cursor()
 
-# Check if the database and user already exist
-DB_EXISTS=$(mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -e "SHOW DATABASES LIKE '$DB_NAME';" | grep "$DB_NAME")
-USER_EXISTS=$(mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -e "SELECT user FROM mysql.user WHERE user='$DB_USER';" | grep "$DB_USER")
+-- Create the database if it doesn't exist
+cursor.execute("CREATE DATABASE IF NOT EXISTS hbnb_dev_db")
 
-# Create the database if it doesn't exist
-if [ -z "$DB_EXISTS" ]; then
-    mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $DB_NAME;"
-    echo "Database $DB_NAME created."
-else
-    echo "Database $DB_NAME already exists."
-fi
+-- Create the user if it doesn't exist and set the password
+cursor.execute("CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'")
 
-# Create the user if it doesn't exist
-if [ -z "$USER_EXISTS" ]; then
-    mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-    echo "User $DB_USER created."
-else
-    echo "User $DB_USER already exists."
-fi
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+cursor.execute("GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'")
 
-# Grant privileges to the user
-mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
-mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -e "GRANT SELECT ON performance_schema.* TO '$DB_USER'@'localhost';"
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+cursor.execute("GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'")
 
-# Flush privileges
-mysql -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+-- Flush privileges to apply the changes
+cursor.execute("FLUSH PRIVILEGES")
 
-echo "Script executed successfully."
+-- Close the cursor and database connection
+cursor.close()
+db.close()
 
